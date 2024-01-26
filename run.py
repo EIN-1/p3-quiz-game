d6a9e2ab852c58a9fa4557187683a0e0e81f1cfd
EIN-1 <>
2024-01-26 03:35:46 +0000

    Add my modified run.py file.

--- run.py
+++ run.py
@@ -11,9 +11,12 @@ colorama.init(autoreset=True)
 
 # introduction and rules
 print(f"\n{Fore.GREEN}WELCOME TO THE PYTHON QUIZ GAME\n")
+print('******************************************************\n')
+
 print(f"\n{Fore.BLUE}Please select the correct answer. For each question "
       f"answered correctly within 15 seconds, you will earn 10 points. "
       f"If it takes longer than that, you earn 5 points:\n\n")
+print('******************************************************\n')
 
 
 # which country are you in
@@ -27,8 +30,10 @@ def location():
         if country:
             print(f"{Fore.GREEN}{country.name}\n")
             break  # Exit the loop once a valid country is found
+            print('******************************************************\n')
         else:
             print(f"{Fore.RED}Error, type in your country.\n")
+            print('******************************************************\n')
 
 
 # Call the location function to prompt for input
@@ -43,6 +48,7 @@ def username():
     # Ensure that username is not empty
     if user_name.strip():
         print(f"{Fore.GREEN}Hello {user_name}, welcome to our quiz game!\n")
+        print('******************************************************\n')
 
 
 # Call the username function to prompt for input
@@ -80,10 +86,15 @@ def quiz(questions):
         answer = input(f"{Fore.BLUE}Your answer: ")
 
         if int(answer) == question["answer"]:
-            print(f"{Fore.GREEN}Correct! 10 points{Fore.WHITE}")
+            print(f"{Fore.GREEN}Correct! 10 points\n")
             score += 10
+            print('******************************************************\n')
         else:
-            print(f"{Fore.RED}Incorrect!{Fore.WHITE}")
+            print(f"{Fore.RED}Incorrect!\n")
+            print('******************************************************\n')
+    print(f"{Fore.BLUE}You scored: {score} \
+        {Fore.GREEN}out of {len(questions) * 10}\n")
+    print('******************************************************\n')
 
 
 quiz(questions)
@@ -101,8 +112,10 @@ score = 0
 
 
 # choice to exit or replay
-print(f"{Fore.YELLOW}Do you want to play again(YES/NO)?")
+print(f"{Fore.YELLOW}Do you want to play again(YES/NO)?\n")
+print('******************************************************\n')
 
 
 # game over message
-print(f"{Fore.YELLOW}Goodbye, see you soon!")
+print(f"{Fore.YELLOW}Goodbye, see you soon!\n")
+print('******************************************************\n')
