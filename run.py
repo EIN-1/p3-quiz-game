afa26cc7971c16878bed06e8b4c64d2c6d98f0e2
EIN-1 <>
2024-01-26 00:48:16 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,42 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# my imports.
+import colorama
+import time
+import pycountry
+import os
+
+from colorama import Fore, Style, Back
+
+colorama.init(autoreset=True)
+
+
+#introduction and gave rules
+print(f"WELCOME TO THE PYTHON QUIZ")
+
+print(f"here are the rules")
+
+
+# which country are you in
+def location():
+
+
+# user name
+def username():
+
+
+# multi-questions quiz with options and answers numbers
+def quiz(questions):
+
+
+# validating error in answering, not to use strings
+def validate( answers):
+
+# If you use less than 15seconds you earn 10 points else you get 5 points
+timer()
+
+score = 0
+
+# choice to exit or replay
+print(f" Do you want to play again(YES/NO)?")
+
+# game over message
+print(f" Thank you for playing, see you soon!")
